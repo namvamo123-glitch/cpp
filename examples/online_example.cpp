dd2a20a33934215ddda0ab185331f37b69ec124d
namvamo123-glitch <namvamo123@gmail.com>
2026-09-07 11:50:25 +0000

    new text

--- examples/online_example.cpp
+++ examples/online_example.cpp
@@ -10,7 +10,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    std::cout<<"Hello World\nThis is Nam\n";
+    std::cout<<"Hello World\nThis is Nam\nChương trình này được viết bằng C++. Và tôi rất tự hào về thành quả của mình!";
 
     return 0;
 }
