9067a29427fd8fa9bfa1d086d1524f924edcd1c7
namvamo123-glitch <namvamo123@gmail.com>
2026-09-07 11:32:05 +0000

    updated from the codespace

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!\n";
+    std::cout << "Hello, world! This is the first program from Nam\nChương trình này được viết bằng C++.\n";
     return 0;
 }
